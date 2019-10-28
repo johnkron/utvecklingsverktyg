08da125e91454d43e26ceae0d05d5a176be74d5a
Adele Engström <adele.engstrom@outlook.com>
2019-10-28 14:21:31 +0200

    new calculator

--- calculator/main.js
+++ calculator/main.js
@@ -5,7 +5,34 @@ const prompt = require('prompt-sync')()
 const run = () => {
 
     let number1 = prompt('Number1:')
-
+    let operator = prompt('input operator')
+    let number2 = prompt('Number2:')
+    number1 = parseInt(number1)
+    number2 = parseInt(number2)
+    if (operator == '+') {
+        let result = number1 + number2 
+        let text = number1 + '+' + number2 + '=' + result
+        console.log(text)
+        console.log(result) 
+    }
+    else if (operator == '-') {
+        let result = number1 - number2
+        let text = number1 + '-' + number2 + '=' + result
+        console.log(text)
+        console.log(result)
+    }
+    else if (operator == '*') {
+        let result = number1 * number2
+        let text = number1 + '*' + number2 + '=' + result
+        console.log(text)
+        console.log(result)
+    } 
+   else if (operator == '/') {
+       let result = number1 / number2
+       let text = number1 + '/' + number2 + '=' + result
+       console.log(text)
+       console.log(result)
+   }
 }
 
 run()
